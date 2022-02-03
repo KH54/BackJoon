45ad496a0400a562a78c0e791379a22150825c05
khPark <pkh543@naver.com>
2022-02-03 17:36:45 +0900

    2022/02/03
    String to ASCII Code
    KH Park

---
+++ BackJoon/src/backJoon/step7/문자열/아스키코드/Main.java
@@ -0,0 +1,17 @@
+package backJoon.step7.문자열.아스키코드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println(br.read());
+        br.close();
+    }
+}
