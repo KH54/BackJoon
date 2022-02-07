75fe37c05f1d24fb2e664f43d163c594cfdb5243
khPark <pkh543@naver.com>
2022-02-07 18:00:26 +0900

    22/02/07
    손익분기점 계산 방법
    KH Park

---
+++ BackJoon/src/backJoon/step8/기본수학1/손익분기점/Main.java
@@ -0,0 +1,26 @@
+package backJoon.step8.기본수학1.손익분기점;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // A = 고정 비용
+        // B = 가변 비용
+        // C = 노트북 가격
+
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        long C = Integer.parseInt(st.nextToken());
+
+        System.out.println( B < C ? A / (C-B)+1 : -1);
+        
+    }
+}
