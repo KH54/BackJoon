fac287f6b6723f1c9034926ba714e1d40e85228d
khPark <pkh543@naver.com>
2022-02-22 17:57:08 +0900

    22/02/22
    피타고라스의 정리로 직각삼각형 찾기
    KH Park

---
+++ BackJoon/src/backJoon/step9/기본수학2/직각삼각형/Main.java
@@ -0,0 +1,38 @@
+package backJoon.step9.기본수학2.직각삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+        int[] arr = new int[3];
+
+        do {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            arr[2] = Integer.parseInt(st.nextToken());
+            Arrays.sort(arr);
+
+            if (arr[2] != 0)
+                if (Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2)) {
+                    sb.append("right");
+                    sb.append("\n");
+                } else {
+                    sb.append("wrong");
+                    sb.append("\n");
+                }
+        } while (arr[0] != 0 && arr[1] != 0 && arr[2] != 0);
+        System.out.println(sb);
+    }
+}
