edc168dd563fa9c0f3f0379c256c23881f285fd5
khPark <pkh543@naver.com>
2022-02-24 17:35:03 +0900

    22/02/24
    에라토스테네스의 체, 소수 구하는 문제
    KH Park

---
+++ BackJoon/src/backJoon/step9/기본수학2/베르트랑공준/Main.java
@@ -0,0 +1,59 @@
+package backJoon.step9.기본수학2.베르트랑공준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static boolean arr[];
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int cnt = 0;
+        int N = Integer.MAX_VALUE;
+        if (N >= 2) {
+            while (true) {
+                cnt = 0;
+                N = Integer.parseInt(br.readLine());
+
+                if (N == 0) {
+                    break;
+                }
+
+                prime(N);
+
+                for (int i = N+1; i < arr.length; i++) {
+                    if (arr[i] == false) {
+                        cnt++;
+                    }
+                }
+                sb.append(cnt);
+                sb.append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    public static void prime(int N) {
+        N = N * 2;
+        arr = new boolean[N + 1];
+
+        if (N < 2) {
+            return;
+        }
+        arr[0] = arr[1] = true;
+
+        for (int i = 2; i * i <= N; i++) {
+            if (arr[i] == true) {
+                continue;
+            }
+            for (int j = i * i; j < arr.length; j = j + i) {
+                arr[j] = true;
+            }
+        }
+    }
+
+}
--- BackJoon/src/backJoon/step9/기본수학2/소인수분해/Main.java
+++ BackJoon/src/backJoon/step9/기본수학2/소인수분해/Main.java
@@ -10,76 +10,89 @@ public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        if (prime(N) == 0) {
-            primeList(N);
-            System.out.println(primeFactory(N));
-
-        } else {
-            System.out.println(N);
-        }
-
-    }
-
-    // 소수인지 확인
-    public static int prime(int N) {
-
-        if (N <= 1) {
-            return 0;
-        }
-
-        for (int i = 2; i < Math.sqrt(N); i++) {
-            if (N % i == 0) {
-                return 0;
-            }
-        }
-
-        return N;
-    }
-
-    // 소수들의 배열
-    public static void primeList(int N) {
-        arr = new boolean[N + 1];
-
-        arr[0] = arr[1] = true;
-        if (N < 2) {
-            return;
-        }
-
-        for (int i = 2; i < Math.sqrt(N); i++) {
-
-            if (arr[i] == true) {
-                continue;
-            }
-
-            for (int j = i * i; j < arr.length; j = j + i) {
-                arr[j] = true;
-            }
-        }
-    }
-
-    public static StringBuilder primeFactory(int N) {
-        // N = 72
         StringBuilder sb = new StringBuilder();
-        int i = 0;
-        while (N != 1) {
-            if (arr[i] == false) {
-                if (N % i == 0) {
-                    N = N / i;
-                    sb.append(i);
-                    sb.append("\n");
-                } else {
-                    i++;
-                }
-            } else {
-                i++;
+        int N = Integer.parseInt(br.readLine());
+        // 다시해보기, N 보다 작은수로 나누기
+
+        for (int i = 2; i * i <= N; i++) {
+            while (N % i == 0) {
+                N /= i;
+                sb.append(i);
+                sb.append("\n");
             }
-
         }
+        if( N != 1) {
+            sb.append(N);
+        }
+        System.out.println(sb);
 
-        return sb;
+//
+//        if (prime(N) == 0) {
+//            primeList(N);
+//            System.out.println(primeFactory(N));
+//
+//        } else {
+//            System.out.println(N);
+//        }
+//
+//    }
+//
+//    // 소수인지 확인
+//    public static int prime(int N) {
+//
+//        if (N <= 1) {
+//            return 0;
+//        }
+//
+//        for (int i = 2; i <= Math.sqrt(N); i++) {
+//            if (N % i == 0) {
+//                return 0;
+//            }
+//        }
+//
+//        return N;
+//    }
+//
+//    // 소수들의 배열
+//    public static void primeList(int N) {
+//        arr = new boolean[N + 1];
+//
+//        arr[0] = arr[1] = true;
+//        if (N < 2) {
+//            return;
+//        }
+//
+//        for (int i = 2; i < Math.sqrt(N); i++) {
+//
+//            if (arr[i] == true) {
+//                continue;
+//            }
+//
+//            for (int j = i * i; j < arr.length; j = j + i) {
+//                arr[j] = true;
+//            }
+//        }
+//    }
+//
+//    public static StringBuilder primeFactory(int N) {
+//        // N = 72
+//        StringBuilder sb = new StringBuilder();
+//        int i = 0;
+//        while (N != 1) {
+//            if (arr[i] == false) {
+//                if (N % i == 0) {
+//                    N = N / i;
+//                    sb.append(i);
+//                    sb.append("\n");
+//                } else {
+//                    i++;
+//                }
+//            } else {
+//                i++;
+//            }
+//
+//        }
+//        return sb;
     }
 
 }
