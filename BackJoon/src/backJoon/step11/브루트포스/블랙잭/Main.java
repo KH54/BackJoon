ca5b5a0bad315ebd7a9333cd9378fea2f0b09a72
khPark <pkh543@naver.com>
2022-02-28 21:59:18 +0900

    22/02/28
    브루트포스 블랙잭
    KH park 미완성

---
+++ BackJoon/src/backJoon/step11/브루트포스/블랙잭/Main.java
@@ -0,0 +1,24 @@
+package backJoon.step11.브루트포스.블랙잭;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        
+    }
+
+}
