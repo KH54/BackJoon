9ab679a21fc7b196f01f3ce65b51d903e52ee6c7
khPark <pkh543@naver.com>
2022-03-02 11:37:37 +0900

    22/03/02
    Test Code
    KH Park

---
+++ BackJoon/src/backJoon/Test.java
@@ -0,0 +1,11 @@
+package backJoon;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int N = 9;
+        System.out.println(N/2);
+        System.out.println(N%2);
+    }
+
+}
