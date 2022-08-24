69ca99dffa72c23b343659e2155a7e64217a0947
khPark <pkh543@naver.com>
2022-08-24 23:50:55 +0900

    22/08/24

--- BackJoon/src/backJoon/stack/스택수열/Main.java
+++ BackJoon/src/backJoon/stack/스택수열/Main.java
@@ -10,33 +10,36 @@ public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         
         int N = Integer.parseInt(br.readLine());
-     
-        Stack<Integer> stack = new Stack<>()
-        
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        
-        stack.push(5);
-        stack.push(6);
-        
-        System.out.println(stack.pop());
-        
-        stack.push(7);
-        stack.push(8);
-        
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        
+        Stack<Integer> stack = new Stack<>();
+
+        int now = 1;
+
+        while (N-- > 0) {
+
+            int value = Integer.parseInt(br.readLine());
+
+            if (value > now) {
+                for (int i = now + 1; i <= value; i++) {
+                    stack.push(i);
+                    sb.append("+\n");
+                }
+                
+                now = value;
+            }
+
+            else if(stack.peek() != value) {
+                System.out.println("NO");
+                return;
+            }
+            
+            stack.pop();
+            sb.append("-\n");
+            
+        }
+        System.out.println(sb);
     }
 
 }
